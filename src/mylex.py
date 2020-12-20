3b1602b5184a996d210958452ff29931029fc464
wanghuikai <kaicasper8927@gmail.com>
2020-12-20 14:01:38 +0800

    Simple tree ok

--- src/mylex.py
+++ src/mylex.py
@@ -1,8 +1,11 @@
 import ply.lex as lex
 import ply.yacc as yacc
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_agraph import graphviz_layout
+
 
 # === Lexical tokens component ===
-
 
 reserved = {
     'if' : 'IF',
@@ -87,7 +90,7 @@ precedence = (
     ('left', 'ELSE'),
     ('left', 'PLUS', 'MINUS'),
     ('left', 'TIMES', 'DIVIDE', 'MODULO'),
-    ('right', 'UMINUS'),
+    ('right', 'UMINUS','POWER','SQUARE'),
 )
 
 # dictionary of names (for storing variables)
@@ -255,10 +258,85 @@ def p_error(p):
 # build parser
 yacc.yacc()
 
+
+# TAC 
+def find_top_prio(lst):
+    top_prio = 1
+    count_ops = 0
+    for ops in lst:
+        if ops in prio_dict:
+            count_ops += 1
+            if prio_dict[ops] > 1:
+                top_prio = prio_dict[ops]
+
+    return top_prio, count_ops
+
+
+
 # start interpreter and accept input using commandline/console
 while True:
     try:
         s = input('calc > ')  # get user input. use raw_input() on Python 2
+        ip_str = s
+        ip_lst = list(map(str,ip_str))
     except EOFError:
         break
+
     yacc.parse(s)  # parse user input string
+    
+    
+    prio_dict = {'-':1,'+':2,'*':3,'/':4,'**':5,'^':6}
+    op_lst = []
+    op_lst.append(['op','arg1','arg2','result'])
+
+    top_prio, count_ops = find_top_prio(ip_lst)
+    ip = ip_lst
+    i, res = 0, 0
+    while i in range(len(ip)):
+      if ip[i] in prio_dict:
+        op = ip[i]
+        if (prio_dict[op]>=top_prio) and (ip[i+1] in prio_dict):
+            res += 1
+            op_lst.append([ip[i+1],ip[i+2],' ','t'+str(res)])
+            ip[i+1] = 't'+str(res)
+            ip.pop(i+2)
+            i = 0
+            top_prio, count_ops = find_top_prio(ip)
+        elif prio_dict[op]>=top_prio:
+            res += 1
+            op_lst.append([op,ip[i-1],ip[i+1],'t'+str(res)])
+            ip[i] = 't'+str(res)
+            ip.pop(i-1)
+            ip.pop(i)
+            i = 0
+            top_prio, count_ops = find_top_prio(ip)
+      if len(ip) == 1:
+        op_lst.append(['=',ip[i],' ','a'])
+        print(op_lst)
+ 
+        G = nx.DiGraph()
+        G.clear()
+        data = op_lst
+        for i in range(1,len(data)-1):
+          if(data[i][1]==data[i][2]):
+            data[i][1] = "L_" + data[i][1]
+            data[i][2] = "R_" + data[i][2]
+
+          G.add_node("%s" %(data[i][1]))
+          G.add_node("%s" %(data[i][2]))
+          G.add_node("%s" %(data[i][3]))
+
+          G.add_edge("%s" %(data[i][3]), "%s" %(data[i][1]))
+          G.add_edge("%s" %(data[i][3]), "%s" %(data[i][2]))
+        
+        nx.nx_agraph.write_dot(G,'test.dot')
+        plt.title('draw_networkx')
+        pos = graphviz_layout(G, prog='dot')
+        nx.draw(G, pos, with_labels=True, arrows=False, node_size=600)
+
+        plt.savefig('nx_test.png')
+        plt.clf()
+      i += 1
+    
+
+
